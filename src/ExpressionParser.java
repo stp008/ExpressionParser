aff0077359887c5b929ad6fe3a597a0bbf11113c
stp008 <clack008@gmail.com>
2014-08-16 01:04:19 +0400

    adding logging

--- src/ExpressionParser.java
+++ src/ExpressionParser.java
@@ -11,7 +11,7 @@ public class ExpressionParser {
 	public static void main(String[] args) {
 		Stack<String> operations = new Stack<String>();
 		Stack<Double> values = new Stack<Double>();
-		String exp = "( 11 + ( ( exp ( ( 2.010635 + ( sin ( ( PI / 2 ) ) * 3 ) ) ) + 50 ) ) / 2 )";
+		String exp = "( 11 + ( ( exp ( ( 2.010635 + ( sin ( ( PI / 2 ) ) * 3 ) ) ) + 50 ) / 2 ) )";
 		// String exp = "( 10 + 2 )";
 		String[] chars = exp.split(" ");
 		for (int i = 0; i < chars.length; i++) {
@@ -20,66 +20,66 @@ public class ExpressionParser {
 				;
 			else if (character.equals("+")) {
 				operations.push(character);
-				log.info("Добавление операции: +");
+				log.debug("Добавление операции: +");
 			} else if (character.equals("-")) {
 				operations.push(character);
-				log.info("Добавление операции: -");
+				log.debug("Добавление операции: -");
 			} else if (character.equals("*")) {
 				operations.push(character);
-				log.info("Добавление операции: *");
+				log.debug("Добавление операции: *");
 			} else if (character.equals("/")) {
 				operations.push(character);
-				log.info("Добавление операции: /");
+				log.debug("Добавление операции: /");
 			} else if (character.equals("sqrt")) {
 				operations.push(character);
-				log.info("Добавление операции: sqrt");
+				log.debug("Добавление операции: sqrt");
 			} else if (character.equals("cos")) {
 				operations.push(character);
-				log.info("Добавление операции: cos");
+				log.debug("Добавление операции: cos");
 			} else if (character.equals("sin")) {
 				operations.push(character);
-				log.info("Добавление операции: sin");
+				log.debug("Добавление операции: sin");
 			} else if (character.equals("exp")) {
 				operations.push(character);
-				log.info("Добавление операции: exp");
+				log.debug("Добавление операции: exp");
 			} else if (character.equals("PI")) {
 				values.push(Math.PI);
-				log.info("Добавление значения: PI");
+				log.debug("Добавление значения: PI");
 			} else if (character.equals("E")) {
 				values.push(Math.E);
-				log.info("Добавление значения: E");
+				log.debug("Добавление значения: E");
 			} else if (character.equals(")")) {
 				String operation = operations.pop();
 				double v = values.pop();
 				if (operation.equals("+")) {
 					v = values.pop() + v;
-					log.info("Вычисленная сумма: " + v);
+					log.debug("Вычисленная сумма: " + v);
 				} else if (operation.equals("-")) {
 					v = values.pop() - v;
-					log.info("Вычисленная разность: " + v);
+					log.debug("Вычисленная разность: " + v);
 				} else if (operation.equals("*")) {
 					v = values.pop() * v;
-					log.info("Вычисленное умножение: " + v);
+					log.debug("Вычисленное умножение: " + v);
 				} else if (operation.equals("/")) {
 					v = values.pop() / v;
-					log.info("Вычисленное частное " + v);
+					log.debug("Вычисленное частное " + v);
 				} else if (operation.equals("sqrt")) {
 					v = Math.sqrt(v);
-					log.info("Вычисленный квадратный корень: " + v);
+					log.debug("Вычисленный квадратный корень: " + v);
 				} else if (operation.equals("cos")) {
 					v = Math.cos(v);
-					log.info("Вычисленный коснус: " + v);
+					log.debug("Вычисленный коснус: " + v);
 				} else if (operation.equals("sin")) {
 					v = Math.sin(v);
-					log.info("Вычисленный синус: " + v);
+					log.debug("Вычисленный синус: " + v);
 				} else if (operation.equals("exp")) {
 					v = Math.exp(v);
-					log.info("Вычисленная экспонента: " + v);
+					log.debug("Вычисленная экспонента: " + v);
 				}
 				values.push(v);
 			} else {
 				values.push(Double.parseDouble(character));
-				log.info("Добавление значения: "
+				log.debug("Добавление значения: "
 						+ Double.parseDouble(character));
 			}
 		}
